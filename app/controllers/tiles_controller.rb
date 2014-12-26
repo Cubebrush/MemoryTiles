d0de22e09da6cd13be4698d56384dbb6d18a0d1a
Marc Brunet <marc@cubebrush.com>
2014-12-26 15:27:05 -0800

    5 to 20 per page

--- app/controllers/tiles_controller.rb
+++ app/controllers/tiles_controller.rb
@@ -6,7 +6,7 @@ class TilesController < ApplicationController
   respond_to :html
 
   def index
-    @tiles = Tile.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+    @tiles = Tile.order("created_at DESC").paginate(:page => params[:page], :per_page => 20)
     respond_with(@tiles)
   end
 
