15b10d90a252920bfbb3b2d094c1c7fa7aa87f20
Marc Brunet <marc@cubebrush.com>
2014-12-26 13:58:21 -0800

    Added Icons and updated show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :tiles
   devise_for :users
-  root 'pages#home'
+  root 'tiles#index'
   get 'about' => 'pages#'
 
   # The priority is based upon order of creation: first created -> highest priority.
